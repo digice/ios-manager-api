e9c188708fa5af31d735919fd4b341d1bb3c1fa1
Roderic Linguri <linguri@digices.com>
2017-04-11 10:56:35 -0600

    Create index.php

---
+++ php/index.php
@@ -0,0 +1,14 @@
+<?php
+
+/** @package    iOS Manager API
+  * @descr      API Server for an iOS Application
+  * @author     Roderic Linguri <linguri@digices.com>
+  * @license    MIT
+  * @copyright  2017 Digices LLC. All Rights Reserved.
+  **/
+
+/** Application Bootstrapper **/
+
+require_once('app/autoload.php');
+
+$ctl = new AppCtl();
